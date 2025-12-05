1cfb72a2517e7b131677c6ea5be793bf55456342
Your Name <aymanashraf201555@gmail.com>
2025-12-05 22:01:19 +0200

    Resolved merge conflict

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,11 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-<<<<<<< HEAD
-    // Add the Google services Gradle plugin
+    // Add the Google services Gradle plugin for Firebase
     id("com.google.gms.google-services")
-=======
->>>>>>> f099a548568129d8536f635149133ad46a1f80fe
 }
 
 android {
@@ -25,10 +22,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID
         applicationId = "com.example.my_first_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -37,8 +32,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
